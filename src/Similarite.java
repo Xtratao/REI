1373958a15f7d5dc15b8cc3dd6abd963cbb8e091
hamzabachar <hamzabachar@gmail.com>
2015-10-27 01:22:40 +0100

    Sort by Value of ranks

--- src/Similarite.java
+++ src/Similarite.java
@@ -4,108 +4,143 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-
-import tools.FrenchStemmer;
-
+import java.util.TreeMap;
 
 public class Similarite {
-	
+
 	/**
 	 * Le répertoire du corpus
 	 */
 	protected static String DIRNAME = "lemonde-utf8";
-	
+
 	/**
 	 * Le fichier contenant les mots vides
 	 */
 	private static String STOPWORDS_FILENAME = "./frenchST.txt";
 
+	// private String fileName;
+	// private double score;
+	//
+	// public Similarite(String n, int s) {
+	// this.fileName = n;
+	// this.score = s;
+	// }
+	//
+	// public String getName() {
+	// return fileName;
+	// }
+	//
+	// public void setName(String name) {
+	// this.fileName = name;
+	// }
+	//
+	// public double getSalary() {
+	// return score;
+	// }
+	//
+	// public void setSalary(int salary) {
+	// this.score = score;
+	// }
+	//
+	// public String toString() {
+	// return "Name: " + this.fileName + "-- Salary: " + this.score;
+	// }
+
 	/**
-	 * renvoi le score de similarité entre les deux documents en utilisant la formule de cos
-	 * les deux documents contiennent les poids (tfIdf) de deux documents du corpus
-	 * @param: file1, file2: files .poids stocking the words of a document and their tfidf weights
-	 * @return: the cosinus similarity of the two files 
+	 * renvoi le score de similarité entre les deux documents en utilisant la
+	 * formule de cos les deux documents contiennent les poids (tfIdf) de deux
+	 * documents du corpus
+	 * 
+	 * @param: file1, file2: files .poids stocking the words of a document and
+	 *         their tfidf weights
+	 * @return: the cosinus similarity of the two files
 	 */
-	
-	public static double getSimilarity(File file1, File file2) throws IOException
-	{	
+
+	public static double getSimilarity(File file1, File file2)
+			throws IOException {
 
 		BufferedReader f1 = new BufferedReader(new FileReader(file1));
 		BufferedReader f2 = new BufferedReader(new FileReader(file2));
-		
+
 		HashMap<String, Double> weightsFile1 = new HashMap<String, Double>();
 		HashMap<String, Double> weightsFile2 = new HashMap<String, Double>();
-		String line="";
-		
+		String line = "";
+
 		// On initiliase les variables qui composent la formule du cosinus
-		
+
 		Double produitScalaire = 0.0; // nominateur
-		Double normePoids1 = 0.0, normePoids2 = 0.0; //dénominateur
-		
-		while((line=f1.readLine())!=null)
-			weightsFile1.put(line.split("\t")[0], Double.parseDouble(line.split("\t")[1]));
-		
-		while((line=f2.readLine())!=null)
-			weightsFile2.put(line.split("\t")[0], Double.parseDouble(line.split("\t")[1]));
-		
-		for (Map.Entry<String, Double> w1 : weightsFile1.entrySet())
-		{
-			if(weightsFile2.containsKey(w1.getKey()))
-			{
-				produitScalaire += w1.getValue()*weightsFile2.get(w1.getKey());
+		Double normePoids1 = 0.0, normePoids2 = 0.0; // dénominateur
+
+		while ((line = f1.readLine()) != null)
+			weightsFile1.put(line.split("\t")[0],
+					Double.parseDouble(line.split("\t")[1]));
+
+		while ((line = f2.readLine()) != null)
+			weightsFile2.put(line.split("\t")[0],
+					Double.parseDouble(line.split("\t")[1]));
+
+		for (Map.Entry<String, Double> w1 : weightsFile1.entrySet()) {
+			if (weightsFile2.containsKey(w1.getKey())) {
+				produitScalaire += w1.getValue()
+						* weightsFile2.get(w1.getKey());
 			}
 		}
-		
-				
+
 		for (Map.Entry<String, Double> w1 : weightsFile1.entrySet())
 			normePoids1 += Math.pow(w1.getValue(), 2);
-			
-		
+
 		for (Map.Entry<String, Double> w2 : weightsFile2.entrySet())
 			normePoids2 += Math.pow(w2.getValue(), 2);
-		
 
 		normePoids1 = Math.sqrt(normePoids1);
 		normePoids2 = Math.sqrt(normePoids2);
 
-		return produitScalaire/(normePoids1*normePoids2);
+		return produitScalaire / (normePoids1 * normePoids2);
 	}
-	
+
 	/**
-	 * renvoi le score de similarité entre un document et l’ensemble des documents du corpus
-	 * @param: file : file .poids stocking the words of a document and its tfidf weights
-	 * @param dir : the directory of all the .poids files of the corpus
-	 * @throws IOException 
+	 * renvoi le score de similarité entre un document et l’ensemble des
+	 * documents du corpus
+	 * 
+	 * @param: file : file .poids stocking the words of a document and its tfidf
+	 *         weights
+	 * @param dir
+	 *            : the directory of all the .poids files of the corpus
+	 * @throws IOException
 	 */
-	
-	public static TreeSet<String> getSimilarDocuments(File file, File dir) throws IOException
-	{
-		
+
+	public static Map<String, Double> getSimilarDocuments(File file,File dir) throws IOException {
+
+        Map<String, Double> map = new HashMap<String, Double>();
+        Map<String, Double> sortedMapAsc = new HashMap<String, Double>();
 		if (dir.isDirectory()) {
 			String[] fileNames = dir.list();
-			
-			System.out.print("Scores du similarité entre le fichier "+file.getName() +" et l'ensemble");
-			System.out.println(" des fichiers du répertoire: "+dir.getName());
+
+			System.out.print("Scores du similarité entre le fichier "+ file.getName() + " et l'ensemble");
+			System.out.println(" des fichiers du répertoire: " + dir.getName());
 			System.out.println();
-		
-			for (String fileName : fileNames) 
-				{
-					
-					System.out.println(fileName +"\t"+ getSimilarity(file, new File(dir.getAbsolutePath()+"/"+fileName)));
-				}
-			
+			double score = 0;
+			for (String fileName : fileNames) {
+				score = getSimilarity(file, new File(dir.getAbsolutePath()+ "/" + fileName));
+				map.put(fileName, score);
+//				System.out.println(fileName+ "\t"+ getSimilarity(file, new File(dir.getAbsolutePath()+ "/" + fileName)));
+			}
+			sortedMapAsc = SortByValue.sortByComparator(map, SortByValue.DESC);
 		}
-		return null;
+		
+		return sortedMapAsc;
 	}
-	
-	public static void main(String[] args) throws IOException 
-	{
-		//System.out.println(getSimilarity(new File(DIRNAME_Mouhcine+"/../outTfIdf/texte.95-1.poids"),new File(DIRNAME_Mouhcine+"/../outTfIdf/texte.95-1.poids")));
-		getSimilarDocuments(new File(DIRNAME+"/../outTfIdf/texte.95-1.poids"),new File(DIRNAME+"/../outTfIdf"));
+
+	public static void main(String[] args) throws IOException {
+		// System.out.println(getSimilarity(new
+		// File(DIRNAME_Mouhcine+"/../outTfIdf/texte.95-1.poids"),new
+		// File(DIRNAME_Mouhcine+"/../outTfIdf/texte.95-1.poids")));
+		Map<String, Double> map = getSimilarDocuments(new File(DIRNAME + "/../outTfIdf/texte.95-1.poids"), new File(DIRNAME + "/../outTfIdf"));
+		SortByValue.printMap(map);
 	}
-	
+
 }
---
+++ src/SortByValue.java
@@ -0,0 +1,79 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SortByValue
+{
+    public static boolean ASC = true;
+    public static boolean DESC = false;
+
+    public static void main(String[] args)
+    {
+
+        // Creating dummy unsorted map
+        Map<String, Double> unsortMap = new HashMap<String, Double>();
+        unsortMap.put("B", 5.5);
+        unsortMap.put("A", 8.0);
+        unsortMap.put("D", 2.0);
+        unsortMap.put("C", 7.0);
+
+        System.out.println("Before sorting......");
+        printMap(unsortMap);
+
+        System.out.println("After sorting ascending order......");
+        Map<String, Double> sortedMapAsc = sortByComparator(unsortMap, ASC);
+        printMap(sortedMapAsc);
+
+
+        System.out.println("After sorting descindeng order......");
+        Map<String, Double> sortedMapDesc = sortByComparator(unsortMap, DESC);
+        printMap(sortedMapDesc);
+
+    }
+
+    public static Map<String, Double> sortByComparator(Map<String, Double> unsortMap, final boolean order)
+    {
+
+        List<Entry<String, Double>> list = new LinkedList<Entry<String, Double>>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<String, Double>>()
+        {
+            public int compare(Entry<String, Double> o1,
+                    Entry<String, Double> o2)
+            {
+                if (order)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<String, Double> sortedMap = new LinkedHashMap<String, Double>();
+        for (Entry<String, Double> entry : list)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
+
+    public static void printMap(Map<String, Double> map)
+    {
+        for (Entry<String, Double> entry : map.entrySet())
+        {
+            System.out.println("Key : " + entry.getKey() + " Value : "+ entry.getValue());
+        }
+    }
+}
