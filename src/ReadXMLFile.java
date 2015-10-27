428e82315e4c98eadda01e5b296d16a3dd7aef25
Mouhcine Taoufiq <taoufiq.mouhcine@gmail.com>
2015-10-27 22:18:46 +0100

    Ajout de ReadXMLFile pour extraire les noms des documents de subindex

---
+++ src/ReadXMLFile.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ReadXMLFile {
+	
+	protected static String DIRNAME = "lemonde-utf8";
+	
+	public static ArrayList<String> listDocuments;
+	
+	public static void getDocumentsNames() throws ParserConfigurationException, SAXException, IOException
+	{
+		listDocuments = new ArrayList<String>();
+		
+		File subIndexDir = new File("subindex/2015");
+		String[] directoriesInSubindex = subIndexDir.list();
+		
+		for(String d : directoriesInSubindex)
+		{
+			String[] listXMLFiles = new File("subindex/2015/"+d).list();
+			
+			for(String XMLFile : listXMLFiles)
+			{
+		
+				File fXmlFile = new File("subindex/2015/"+d+"/"+XMLFile);
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(fXmlFile);
+				
+				doc.getDocumentElement().normalize();
+				NodeList nList = doc.getElementsByTagName("doc");
+				
+				for (int i = 0; i < nList.getLength(); i++) 
+				{
+					Node nNode = nList.item(i);
+					Element eElement = (Element) nNode;
+					
+					listDocuments.add(eElement.getAttribute("id")+".txt");
+					System.out.println(eElement.getAttribute("id")+".txt");
+				}
+			}
+		}
+	}
+
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		
+		getDocumentsNames();
+		System.out.println("Number of documents = " + listDocuments.size());
+
+	}
+
+}
