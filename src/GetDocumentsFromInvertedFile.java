5312f3c17c8ff344071bce93a7c4439b1783182b
Mouhcine Taoufiq <taoufiq.mouhcine@gmail.com>
2015-10-28 16:20:50 +0100

    Ajout classe API : GetDocumentsFromInvertedFile

---
+++ src/GetDocumentsFromInvertedFile.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class GetDocumentsFromInvertedFile {
+
+	public static TreeMap<String, TreeSet<String>> invertedFile;
+	
+	public void loadInvertedFile()
+	{
+		
+	}
+	
+	public TreeSet<String> fusion(String s1, String s2)
+	{
+		return null;
+	}
+	
+	public TreeSet<String> fusion(String[] motsRequete)
+	{
+		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
